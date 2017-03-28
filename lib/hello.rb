b2a86a3a82de17ff9d6938d6f1382d760805f398
Dyrell Cole <dcole27@uncc.edu>
2017-03-28 01:36:28 +0000

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,8 @@
+require 'greeter'
+
+# Dyrell Cole 
 # Default is World
-# Author: Dyrell Cole ( dcole27@uncc.edu )
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
